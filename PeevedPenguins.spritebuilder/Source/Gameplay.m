ccac3ba344f4209bcdacb18c79194e5f5cda3009
lokada <lyokada@ucsc.edu>
2014-06-26 15:07:33 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -35,7 +35,6 @@
     _mouseJointNode.physicsBody.collisionMask = @[];
     
     _physicsNode.collisionDelegate = self;
-    self.physicsBody.collisionType = @"seal";
 }
 
 // called on every touch in this scene
