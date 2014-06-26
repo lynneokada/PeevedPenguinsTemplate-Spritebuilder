b175019d54695fa3565552e62763b555581b2af6
lokada <lyokada@ucsc.edu>
2014-06-26 15:49:07 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -141,5 +141,17 @@
 
 - (void)sealRemoved:(CCNode *)seal {
     [seal removeFromParent];
+    
+    //load particle effect
+    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
+    //make the particle effect clean itself up, once it is completed
+    explosion.autoRemoveOnFinish = TRUE;
+    //place the particle effect on the seal position
+    explosion.position = seal.position;
+    //add the particle effect to the same node the seal is on
+    [seal.parent addChild:explosion];
+    
+    //remove the destroyed seal
+    [seal removeFromParent];
 }
 @end
