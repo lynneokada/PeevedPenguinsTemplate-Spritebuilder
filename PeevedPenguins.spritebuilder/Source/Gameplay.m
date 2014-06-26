f8c37d523c44f9acf9871c1366ba4770d76484aa
lokada <lyokada@ucsc.edu>
2014-06-26 00:50:53 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -11,11 +11,15 @@
 @implementation Gameplay {
     CCPhysicsNode *_physicsNode;
     CCNode *_catapultArm;
+    CCNode *_levelNode;
 }
 
 - (void)didLoadFromCCB{
     //tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
+    
+    CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
+    [_levelNode addChild:level];
 }
 
 // called on every touch in this scene
